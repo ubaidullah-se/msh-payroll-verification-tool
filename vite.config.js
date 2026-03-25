07ac681903874fa7c977c9c78a8df10a9b0e3876
Ubaidullah <ubaidmian1234@gmail.com>
2026-03-25 00:00:44 -0600

    added vite obfuscator

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import ViteObfuscator from "vite-plugin-javascript-obfuscator";
 
 export default defineConfig({
   root: "src",
-  plugins: [viteSingleFile()],
+  plugins: [
+    viteSingleFile(),
+    ViteObfuscator({
+      // obfuscate JS
+      options: {
+        compact: true,
+        controlFlowFlattening: true,
+        deadCodeInjection: true,
+        stringArrayEncoding: ["rc4"],
+        disableConsoleOutput: true,
+        debugProtection: true,
+      },
+    }),
+  ],
   build: {
     outDir: "../dist",
     emptyOutDir: true,
